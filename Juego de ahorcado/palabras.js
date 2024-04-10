f142943050489e657db9bb442411120369e976bc
FrancyDaniela <158625837+FrancyDaniela@users.noreply.github.com>
2024-04-10 17:22:48 -0500

    juego sudoku

---
+++ Juego de ahorcado/palabras.js
@@ -0,0 +1,3 @@
+const words = ['Carne','Martillo','Lavadora','Sucio','Cangrejo','Lento','Alimentos','Imagen','Pizza','Perro','Doctor', 
+'Ingeniero','Profesora', 'Hamburguesa','Pollo','Destornillador','Cajon', 'Camioneta','Celular','Computador','Caballo',
+'Limpio','Astuto']
